7df7ead7b11f186b76aa12d8c16f18f20fda89d7
Daniel Barco <dabarcoes@gmail.com>
2020-09-02 20:17:40 -0500

    'Migrations'

--- blog/database/migrations/2020_09_01_012005_create_categories_table.php
+++ blog/database/migrations/2020_09_01_012005_create_categories_table.php
@@ -16,7 +16,7 @@ class CreateCategoriesTable extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
-            $table->string('photo')->default('imgs/no-photo.png');
+            $table->string('photo')->default('imgs/no-category.png');
             $table->text('description');
             $table->timestamps();
         });
