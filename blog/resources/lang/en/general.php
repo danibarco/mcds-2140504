0780d9c3cbaca23ea1c1f35a568a69f8a2f98a2f
Daniel Barco <dabarcoes@gmail.com>
2020-10-05 20:37:35 -0500

    Register

--- blog/resources/lang/en/general.php
+++ blog/resources/lang/en/general.php
@@ -1,6 +1,28 @@
 <?php
 
 return [
-    'link-login'     => 'Login',
-    'link-register'  => 'Register',
+    // Login Page
+    'link-login'     => 'Ingresar',
+    'link-register'  => 'Registrarse',
+    'title-login'    => 'Form Login',
+    'label-email'    => 'E-Mail Address',
+    'label-password' => 'Password',
+    'label-remember' => 'Remember Me',
+    'btn-login'      => 'Login',
+    'link-forgot'	 => 'Forgot Your Password?',
+
+    // Register Page
+    'title-register'  => 'Register',
+    'label-fullname'  => 'FullName',
+    'label-phone'     => 'PhoneNumber',
+    'label-birthdate' => 'BirthDate',
+    'label-gender'    => 'Gender',
+    'select-value'    => 'Select...',
+    'select-female'   => 'Female',
+    'select-male'     => 'Male',
+    'label-address'   => 'Address',
+
+    // Reset Password Page
+    'label-confirm'   => 'Confirm Password',
+    'btn-register'    => 'Register',
 ];
